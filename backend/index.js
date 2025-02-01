7db0118be1cf4b0a473447badff35a75daac421c
SakshamManav <manavsaksham06@gmail.com>
2025-02-01 14:11:34 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const cors = require('cors');
+const cors = require("cors");
 const connectMongoDb = require("./db.js");
-const webSocketConnection = require("./routes/Websocket.js")
+const webSocketConnection = require("./routes/Websocket.js");
 
 connectMongoDb();
 
-
-
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: ["http://192.168.29.16:3000, http://localhost:3000, https://instagram-clone-mocha-zeta.vercel.app"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://instagram-clone-mocha-zeta.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-app.use(cors());
+
 
 app.use("/insta/user", require("./routes/User.js"));
 app.use("/insta/content", require("./routes/Content.js"));
 
-
-const server = app.listen(PORT, '0.0.0.0', () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
-
-
 
 module.exports = server;
 
 // console.log(server)
 webSocketConnection(server);
-
